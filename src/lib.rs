d40eedce32a4f52947264a11afcd9dea5df8af65
Shaan Batra <sb1752@gmail.com>
2022-06-28 20:20:56 -0700

    separate methods for reading 4 bytes and varint

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ pub struct RawTransaction {
 #[allow(dead_code)] // remove warnings that fields are never read
 struct ParsedTransaction {
     version: u32,
+    input_count: u64,
 }
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
@@ -34,17 +35,51 @@ pub fn get_args() -> MyResult<RawTransaction> {
     })
 }
 
+fn read_4_bytes(bytes_slice: &mut &[u8]) -> MyResult<u32> {
+    let mut buffer = [0; 4];
+    bytes_slice.read(&mut buffer)?;
+    Ok(u32::from_le_bytes(buffer)) // little endian
+}
+
+fn read_var_int(bytes_slice: &mut &[u8]) -> MyResult<u64> {
+    let mut marker = [0; 1];
+    bytes_slice.read(&mut marker)?;
+    let num;
+    let marker_hex = hex::encode(marker);
+
+    if marker_hex < hex::encode("0xFD") {
+        num = u8::from_le_bytes(marker) as u64
+    } else if marker_hex == hex::encode("0xFD") {
+        let mut buffer = [0; 2];
+        bytes_slice.read(&mut buffer)?;
+        num = u16::from_le_bytes(buffer) as u64;
+    } else if marker_hex == hex::encode("0xFE") {
+        let mut buffer = [0; 4];
+        bytes_slice.read(&mut buffer)?;
+        num = u32::from_le_bytes(buffer) as u64;
+    } else {
+        let mut buffer = [0; 8];
+        bytes_slice.read(&mut buffer)?;
+        num = u64::from_le_bytes(buffer);
+    }
+
+    Ok(num)
+}
+
 pub fn run(raw_transaction: RawTransaction) -> MyResult<()> {
     let bytes = Vec::<u8>::from_hex(raw_transaction.hex)?;
-    let mut byte_slice = &bytes[..];
+    let mut bytes_slice = &bytes[..];
 
     // version is 4 bytes
-    let mut buffer = [0; 4];
-    byte_slice.read(&mut buffer)?;
-    // convert bytes to integer using little endian
-    let version = u32::from_le_bytes(buffer);
+    let version = read_4_bytes(&mut bytes_slice)?;
 
-    let parsed_tx = ParsedTransaction { version };
+    // input count is var int
+    let input_count = read_var_int(&mut bytes_slice)?;
+
+    let parsed_tx = ParsedTransaction {
+        version,
+        input_count,
+    };
 
     println!("{:?}", parsed_tx);
 
