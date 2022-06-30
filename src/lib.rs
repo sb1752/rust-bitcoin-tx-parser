cc1caca79da03fed63923e8c049b3c688c783440
Shaan Batra <sb1752@gmail.com>
2022-06-30 10:18:29 -0700

    read inputs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use buffer::ReadBuffer;
 use clap::{App, Arg};
 use hex::FromHex;
 use std::error::Error;
@@ -13,6 +14,15 @@ pub struct RawTransaction {
 struct ParsedTransaction {
     version: u32,
     input_count: u64,
+    inputs: Vec<Input>,
+}
+
+#[derive(Debug)]
+struct Input {
+    txid: String,
+    vout: u32,
+    scriptSig: String,
+    sequence: u32,
 }
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
@@ -48,7 +58,7 @@ fn read_var_int(bytes_slice: &mut &[u8]) -> MyResult<u64> {
     let marker_hex = hex::encode(marker);
 
     if marker_hex < hex::encode("0xFD") {
-        num = u8::from_le_bytes(marker) as u64
+        num = u8::from_le_bytes(marker) as u64;
     } else if marker_hex == hex::encode("0xFD") {
         let mut buffer = [0; 2];
         bytes_slice.read(&mut buffer)?;
@@ -66,6 +76,42 @@ fn read_var_int(bytes_slice: &mut &[u8]) -> MyResult<u64> {
     Ok(num)
 }
 
+fn read_transaction(bytes_slice: &mut &[u8]) -> MyResult<String> {
+    let mut buffer = [0; 32];
+    bytes_slice.read(&mut buffer)?;
+    buffer.reverse(); // txids are formatted in big endian
+    Ok(hex::encode(buffer))
+}
+
+fn read_script_sig(bytes_slice: &mut &[u8]) -> MyResult<String> {
+    let mut marker = [0; 1];
+    bytes_slice.read(&mut marker)?;
+    let size = u8::from_le_bytes(marker) as usize;
+
+    let mut buffer = Vec::with_capacity(size);
+    bytes_slice.read_buffer(&mut buffer)?;
+
+    Ok(hex::encode(buffer))
+}
+
+fn read_inputs(bytes_slice: &mut &[u8], input_count: u64) -> MyResult<Vec<Input>> {
+    let mut inputs = vec![];
+    for _ in 0..input_count {
+        let txid = read_transaction(bytes_slice)?;
+        let vout = read_4_bytes(bytes_slice)?;
+        let scriptSig = read_script_sig(bytes_slice)?;
+        let sequence = read_4_bytes(bytes_slice)?;
+
+        inputs.push(Input {
+            txid,
+            vout,
+            scriptSig,
+            sequence,
+        })
+    }
+    Ok(inputs)
+}
+
 pub fn run(raw_transaction: RawTransaction) -> MyResult<()> {
     let bytes = Vec::<u8>::from_hex(raw_transaction.hex)?;
     let mut bytes_slice = &bytes[..];
@@ -76,12 +122,16 @@ pub fn run(raw_transaction: RawTransaction) -> MyResult<()> {
     // input count is var int
     let input_count = read_var_int(&mut bytes_slice)?;
 
+    // read inputs
+    let inputs = read_inputs(&mut bytes_slice, input_count)?;
+
     let parsed_tx = ParsedTransaction {
         version,
         input_count,
+        inputs,
     };
 
-    println!("{:?}", parsed_tx);
+    println!("{:#?}", parsed_tx);
 
     Ok(())
 }
